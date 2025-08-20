5cee3b5b9848ef3547ed1e23127f47cc72da1e07
Tochukwu Charles Ikechukwu <ikechukwutochukwu95@gmail.com>
2025-08-20 11:15:51 +0100

    feat: add the search properties functionality

--- backend/src/models/property.models.ts
+++ backend/src/models/property.models.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-// 1. Define the TypeScript interface for a Property document in MongoDB
+// Define the TypeScript interface for a Property document in MongoDB
 
 export interface Iproperty extends Document {
     title: string;
@@ -12,20 +12,23 @@ export interface Iproperty extends Document {
     createdAt: Date;
 }
 
-// 2. Define the schema (MongoDB structure)
+// Define the schema (MongoDB structure)
 
 const PropertySchema: Schema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
     location: { type: String, required: true },
-    type: { type: String, required: String },
+    type: { type: String, required: true },
     images: { type: [String], default: [] }  // optional array of image URLs
 }, { timestamps: true });
 
-// 3. Export the model so it can be used in controllers
-export default mongoose.model<Iproperty>("Property", PropertySchema)
+// Export the model so it can be used in controllers
+// export default mongoose.model<Iproperty>("Property", PropertySchema)
 
 // Can also use a named export like this
-// const Property = mongoose.model<Iproperty>("Property",PropertySchema)
-// export Property.   but the default one above allows me to import this model with any name.
+const Property = mongoose.model<Iproperty>("Property",PropertySchema)
+export {
+    Property
+}
+//but the default one above allows me to import this model with any name.
