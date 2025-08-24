1f90ce5d0a733d81187cdb3ee7c1b0289388e680
Adeneey-Dev <oyewolesaheed638@gmail.com>
2025-08-24 19:59:34 +0100

    Add-freature/property-model/upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes"; // ✅ import the router
+import propertyRoutes from "./routes/propertyRoutes";
 
 dotenv.config();
 
@@ -13,14 +14,20 @@ const PORT = process.env.PORT || 5000;
 // ✅ use the imported router
 app.use("/api/auth", authRoutes);
 
+//This serve the frontend the uploads image, useful to show the uploaded peoperty images
+app.use("/uploads", express.static("uploads"));
+
+// Import property routes
+app.use("/api/properties", propertyRoutes);
+
 app.get("/", (req: Request, res: Response) => {
   res.send("RealityFinder API is running");
 });
 
-mongoose.connect(process.env.MONGODB_URI || "")
+mongoose
+  .connect(process.env.MONGODB_URL || "")
   .then(() => console.log(`✅ MongoDB Connected`))
-  .catch(err => console.error("❌ MongoDB connection error:", err));
-
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
