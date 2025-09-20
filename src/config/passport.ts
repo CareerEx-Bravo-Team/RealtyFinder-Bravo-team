6362bccd28f7a68ef335e4ba13ada49b82165de9
Tochukwu Charles Ikechukwu <ikechukwutochukwu95@gmail.com>
2025-09-20 23:22:57 +0100

    chore: set up mongoDB database connection

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -13,7 +13,7 @@ dotenv.config();
 // Configure the Google strategy for use by Passport.
 passport.use(new GoogleStrategy(
     {
-        clientID: process.env.GOOGLE_CLIENT_ID as string,
+        clientID: process.env.GOOGLE_CLIENT_ID as string,  
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         callbackURL: process.env.GOOGLE_CALLBACK_URL as string,
     },
